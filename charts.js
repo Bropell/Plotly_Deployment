124b44e8cf54ae4dff130b41f1f16d29a8426e9e
Brandon Ropell <brandon.ropell@gmail.com>
2022-08-21 14:43:29 -0400

    Color testing

--- charts.js
+++ charts.js
@@ -113,7 +113,7 @@ function buildCharts(sample) {
       mode: "markers",
       marker: {
         color: otu_ids,
-        size: sample_values        
+        size: sample_values       
       }  
     };
 
